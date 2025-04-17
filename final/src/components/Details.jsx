e03a31e3c05a057da56e2e8f126e88e7a9612cf7
CHillcode931 <Charleswhill931@gmail.com>
2025-04-17 18:21:03 -0500

    trying to get everything up

--- final/src/components/Details.jsx
+++ final/src/components/Details.jsx
@@ -19,6 +19,7 @@ export const Details = () => {
     });
   }, [id]);
 
+  
   return (
     <>
       <button onClick={() => navigate(-1)}>
--- final/src/context/Search.jsx
+++ final/src/context/Search.jsx
@@ -11,3 +11,4 @@ export const SearchProvider = ({ children }) => {
     </SearchContext.Provider>
   );
 };
+
